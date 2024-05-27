e7cc47a32175aa5b2f0f13089be86826b3336478
Samuel Rodriguez <samuelrodriguez795@gmail.com>
2024-05-27 12:56:43 -0500

    first commit

---
+++ src/main/java/io/riwi/riwimoodle/RiwimoodleApplication.java
@@ -0,0 +1,13 @@
+package io.riwi.riwimoodle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiwimoodleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiwimoodleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/riwi/riwimoodle/RiwimoodleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.riwi.riwimoodle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiwimoodleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
